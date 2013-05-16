b6284087dd3f2a794d70f162623ec7f5463a4497
Bruno Rezende <brunovianarezende@gmail.com>
2013-05-16 11:43:55 -0300

    #2: sentry handler can now decide if it will ignore @ operator or not
    right now the behaviour is to always ignore the @ (error control)
    operator, this change allows the user of the extension to decided if it
    will be the case in his installation or not. The default will be to
    ignore the @ operator and always log errors to sentry.

--- src/app/code/community/AMG/Sentry/functions.php
+++ src/app/code/community/AMG/Sentry/functions.php
@@ -9,5 +9,7 @@
  * @param integer $errline
  */
 function sentry_error_handler($errno, $errstr, $errfile, $errline){
-	Mage::getSingleton('amg-sentry/client')->sendMessage($errstr, sprintf('In %s on line %d', $errfile, $errline));
+    if(error_reporting() != 0 or Mage::getStoreConfigFlag('dev/amg-sentry/ignore-error-control-operator')){
+        Mage::getSingleton('amg-sentry/client')->sendMessage($errstr, sprintf('In %s on line %d', $errfile, $errline));
+    }
 }
